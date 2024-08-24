1c606e344fccf1ba952dc150cc49bd95ca1db1ad
SAK74 <sergijks@gmail.com>
2024-08-24 21:50:05 +0200

    fix: change shadow styling

--- packages/core-components/src/components/alert/alert.ts
+++ packages/core-components/src/components/alert/alert.ts
@@ -1,4 +1,4 @@
-import { html, LitElement, css } from 'lit';
+import { html, LitElement, css, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 import '@shoelace-style/shoelace/dist/components/alert/alert';
@@ -6,28 +6,43 @@ import { Variant } from '../../globals/variants';
 
 import '@shoelace-style/shoelace/dist/components/icon/icon';
 
-// import infoIcon from '../../../../../node_modules/@shoelace-style/shoelace/dist/assets/icons/info-circle.svg';
-// import successIcon from '../../../../../node_modules/@shoelace-style/shoelace/dist/assets/icons/check2-circle.svg';
-// import setupIcon from '../../../../../node_modules/@shoelace-style/shoelace/dist/assets/icons/gear.svg';
-// import warningIcon from '../../../../../node_modules/@shoelace-style/shoelace/dist/assets/icons/exclamation-triangle.svg';
-// import dangerIcon from '../../../../../node_modules/@shoelace-style/shoelace/dist/assets/icons/exclamation-octagon.svg';
-
-// export { successIcon };
-
-// const matchedIcons: { [x in Variant]: string } = {
-//   primary: infoIcon,
-//   success: successIcon,
-//   neutral: setupIcon,
-//   warning: warningIcon,
-//   danger: dangerIcon,
-// };
-
 import '@shoelace-style/shoelace/dist/themes/light.css';
-import '@sak74/design-tokens/css';
+// import '@sak74/design-tokens/css';
 import { ifDefined } from 'lit/directives/if-defined.js';
+
+import {
+  ButtonTextColor,
+  ColorAliasPrimary1,
+  ColorAliasPrimary10,
+  ColorAliasPrimary11,
+  ColorAliasPrimary2,
+  ColorAliasPrimary3,
+  ColorAliasPrimary4,
+  ColorAliasPrimary5,
+  ColorAliasPrimary6,
+  ColorAliasPrimary7,
+  ColorAliasPrimary8,
+  ColorAliasPrimary9,
+} from '@sak74/design-tokens';
 
 @customElement('pp-alert')
 export class Alert extends LitElement {
+  static override styles = css`
+    :host {
+      --sl-color-primary-50: ${unsafeCSS(ColorAliasPrimary1)};
+      --sl-color-primary-100: ${unsafeCSS(ColorAliasPrimary2)};
+      --sl-color-primary-200: ${unsafeCSS(ColorAliasPrimary3)};
+      --sl-color-primary-300: ${unsafeCSS(ColorAliasPrimary4)};
+      --sl-color-primary-400: ${unsafeCSS(ColorAliasPrimary5)};
+      --sl-color-primary-500: ${unsafeCSS(ColorAliasPrimary6)};
+      --sl-color-primary-600: ${unsafeCSS(ColorAliasPrimary7)};
+      --sl-color-primary-700: ${unsafeCSS(ColorAliasPrimary8)};
+      --sl-color-primary-800: ${unsafeCSS(ColorAliasPrimary9)};
+      --sl-color-primary-900: ${unsafeCSS(ColorAliasPrimary10)};
+      --sl-color-primary-950: ${unsafeCSS(ColorAliasPrimary11)};
+      --sl-color-neutral-600: ${unsafeCSS(ButtonTextColor)};
+    }
+  `;
   @property({ reflect: true, type: Boolean }) open: boolean = true;
   @property({ reflect: true, type: Boolean }) closable: boolean = false;
 
@@ -38,8 +53,6 @@ export class Alert extends LitElement {
   @property({ type: String }) descr?: string;
 
   @property({ type: String }) iconSrc?: string;
-
-  static override styles = css``;
 
   protected override render(): unknown {
     return html`
