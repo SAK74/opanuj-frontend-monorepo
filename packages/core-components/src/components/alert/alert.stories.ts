02c186ad05fd135f001e0f9caf74f317c244ca66
SAK74 <sergijks@gmail.com>
2024-08-24 20:47:09 +0200

    fix: fix icon prop passing

--- packages/core-components/src/components/alert/alert.stories.ts
+++ packages/core-components/src/components/alert/alert.stories.ts
@@ -5,11 +5,31 @@ import { html } from 'lit';
 import { variants, Variant } from '../../globals/variants';
 import { ifDefined } from 'lit/directives/if-defined.js';
 
+import infoIcon from '../../../../../node_modules/@shoelace-style/shoelace/dist/assets/icons/info-circle.svg';
+import successIcon from '../../../../../node_modules/@shoelace-style/shoelace/dist/assets/icons/check2-circle.svg';
+import setupIcon from '../../../../../node_modules/@shoelace-style/shoelace/dist/assets/icons/gear.svg';
+import warningIcon from '../../../../../node_modules/@shoelace-style/shoelace/dist/assets/icons/exclamation-triangle.svg';
+import dangerIcon from '../../../../../node_modules/@shoelace-style/shoelace/dist/assets/icons/exclamation-octagon.svg';
+
+export { infoIcon, successIcon, setupIcon, warningIcon, dangerIcon };
+
+import testIconSVG from '../../test.icons/next.svg';
+
 type CustomArgs = {
   open: boolean;
   closable: boolean;
   variant: Variant;
   duration?: number;
+  icon?: string;
+};
+
+const iconMapping = {
+  'Info Icon': infoIcon,
+  'Success Icon': successIcon,
+  'Setup Icon': setupIcon,
+  'Warning Icon': warningIcon,
+  'Danger Icon': dangerIcon,
+  'Custom Icon': testIconSVG,
 };
 
 const meta: Meta<
@@ -30,6 +50,13 @@ const meta: Meta<
     //   control: { type: 'number', min: 500, max: 10000, step: 100 },
     // },
     message: { type: 'string' },
+    icon: {
+      control: {
+        type: 'select',
+      },
+      options: Object.keys(iconMapping),
+      mapping: iconMapping,
+    },
   },
   args: {
     open: true,
@@ -37,8 +64,9 @@ const meta: Meta<
     // closable: true,
     message: 'Exampled alert',
     descr: 'some description',
+    icon: infoIcon,
   },
-  render({ open, variant, closable, duration, message, descr }) {
+  render({ open, variant, closable, duration, message, descr, icon }) {
     return html`
       <pp-alert
         ?open=${open}
@@ -47,6 +75,7 @@ const meta: Meta<
         duration=${ifDefined(duration)}
         message=${message}
         descr=${ifDefined(descr)}
+        iconSrc=${ifDefined(icon)}
       >
         <span slot="describe">${descr}</span>
       </pp-alert>
