3df2b27ae46beb5411d53725c3fcce39d3a4cf8d
SAK74 <sergijks@gmail.com>
2024-08-25 20:28:35 +0200

    fix: update storybook

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,18 @@
-export default { extends: ['@commitlint/config-conventional'] };
+/**
+ * @type {import('@commitlint/types').UserConfig}
+ */
+export default {
+  extends: ['@commitlint/config-conventional'],
+  formatter: '@commitlint/format',
+  rules: {
+    'body-case': [2, 'always', 'lower-case'],
+  },
+  prompt: {
+    messages: {},
+    questions: {
+      type: {
+        description: 'Please input type',
+      },
+    },
+  },
+};
